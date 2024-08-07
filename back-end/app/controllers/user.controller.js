60da9a715bdbd50446102b6f60bed14a5726352b
nguyenhaugiang <giangb2014563@student.ctu.edu.vn>
2024-08-07 21:13:31 +0700

    cai dat xu ly loi va route va controller

---
+++ back-end/app/controllers/user.controller.js
@@ -0,0 +1,23 @@
+exports.create = (req, res) => {
+    res.send({message: 'handle create'});
+};
+
+exports.findALL = (req, res) => {
+    res.send({message: 'handle findALL'});
+};
+
+exports.findOne = (req, res) => {
+    res.send({message: 'handle findOne'});
+};
+
+exports.update = (req, res) => {
+    res.send({message: 'handle update'});
+};
+
+exports.delete = (req, res) => {
+    res.send({message: 'handle delete'});
+};
+
+exports.deleteALL = (req, res) => {
+    res.send({message: 'handle deleteALL'});
+};
